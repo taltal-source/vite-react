bf053cfb36291eba0a868f939f6dda8a6b8ac10e
taltal-source <0taltalsource@gmail.com>
2024-09-07 06:22:42 +0000

    WSLの設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+
+  // WSL用設定
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+
+    // Devcontainerポート転送設定
+    host: '127.0.0.1',
+  },
+});
